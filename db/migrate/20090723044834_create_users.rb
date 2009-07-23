0a8ed55f960141166d79d979b4c92fe661ff96ce
Chris <chris.lerum@gmail.com>
2009-07-23 04:49:03 -0400

    added some basic authlogic stuff, plus acl9. can easily be ripped out in favor of lockdown, if winds blow that way.

---
+++ db/migrate/20090723044834_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :login               , :null => false
+      t.string :email               , :null => false
+      t.string :crypted_password    , :null => false
+      t.string :password_salt       , :null => false
+      t.string :persistence_token   , :null => false
+
+      t.timestamps
+    end
+    add_index :users, :login
+    add_index :users, :persistence_token
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
