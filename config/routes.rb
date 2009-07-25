92d2b781beba355766312a9c14c2707df40a765f
Chris <chris.lerum@gmail.com>
2009-07-25 03:21:46 -0400

    no reason

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
   map.resources :users
   map.resource :user_session
-  map.root :controller => "user_sessions", :action => "new"
+  map.login 'login', :controller => "user_sessions", :action => "new"
+  map.root :controller => "home"
 end
