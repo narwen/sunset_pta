d7d2c3a9c22010e77f909d315fb76330b5d6a4d9
Thomas Jack <thomasjack@gmail.com>
2009-07-29 16:34:57 -0500

    Also revert this authlogic/acl9 stuff since it wasn't behavior-driven.
    This reverts commit 0a8ed55f960141166d79d979b4c92fe661ff96ce.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resource :account, :controller => "users"
-  map.resources :users
-  map.resource :user_session
-  map.root :controller => "user_sessions", :action => "new"
+  map.root :controller => 'home'
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
 end
