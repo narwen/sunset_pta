9b2a305e811998fd859103150d88a998185ee294
Chris <chris.lerum@gmail.com>
2009-07-30 05:46:37 -0400

    added just enough to get first feature passing, something about user logging in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'home'
+  map.login 'login', :controller => "user_sessions", :action => "new"
+  map.resource :account, :controller => "users"
+  map.resources :users
+  map.resource :user_session
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
