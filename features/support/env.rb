9ecf5447294193d6aff3c7f9406d7b9bd878e20c
Thomas Jack <thomasjack@gmail.com>
2009-08-01 17:26:57 -0500

    spork it!

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,25 +1,47 @@
-# Sets up the Rails environment for Cucumber
-ENV["RAILS_ENV"] ||= "cucumber"
-require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
-require 'cucumber/rails/world'
+require 'rubygems'
 
-# Comment out the next line if you don't want Cucumber Unicode support
-require 'cucumber/formatter/unicode'
+prefork = lambda {
+  # Sets up the Rails environment for Cucumber
+  ENV["RAILS_ENV"] = "cucumber"
+  require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
 
-# Comment out the next line if you don't want transactions to
-# open/roll back around each scenario
-Cucumber::Rails.use_transactional_fixtures
+  require 'webrat'
+  # require 'cucumber/webrat/table_locator' # Lets you do table.diff!(table_at('#my_table').to_a)
 
-# Comment out the next line if you want Rails' own error handling
-# (e.g. rescue_action_in_public / rescue_responses / rescue_from)
-Cucumber::Rails.bypass_rescue
+  Webrat.configure do |config|
+    config.mode = :rails
+  end
 
-require 'webrat'
-#require 'cucumber/webrat/table_locator' # Lets you do table.diff!(table_at('#my_table').to_a)
+  require 'webrat/core/matchers'
+  require 'cucumber'
 
-Webrat.configure do |config|
-  config.mode = :rails
+  # Comment out the next line if you don't want Cucumber Unicode support
+  # require 'cucumber/formatter/unicode'
+
+  require 'spec/rails'
+  require 'cucumber/rails/rspec'
+}
+
+each_run = lambda {
+  # This code will be run each time you run your specs.
+  require 'cucumber/rails/world'
+
+  # Comment out the next line if you don't want transactions to
+  # open/roll back around each scenario
+  Cucumber::Rails.use_transactional_fixtures
+
+  # Comment out the next line if you want Rails' own error handling
+  # (e.g. rescue_action_in_public / rescue_responses / rescue_from)
+  Cucumber::Rails.bypass_rescue
+}
+
+# conditionally use spork so windoze can still run the features
+begin
+  require 'spork'
+
+  Spork.prefork(&prefork)
+  Spork.each_run(&each_run)
+rescue LoadError
+  prefork.call
+  each_run.call
 end
-
-require 'cucumber/rails/rspec'
-require 'webrat/core/matchers'
