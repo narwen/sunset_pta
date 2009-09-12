8fa5b0630f6458692c2af22f75d5674753cf09a8
Grace Liu <graceliu@yahoo.com>
2009-09-11 23:26:12 -0700

    updated to add more chair-related functionalities in User for board member list story

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ActiveRecord::Base
   
   has_many :assignments, :dependent => :destroy
   has_many :committees, :through => :assignments
+  has_many :chaired_committees, :through => :assignments, :source => :committee,
+    :conditions => "assignments.duty_id = '" + Duty.find_by_name("Chair").id.to_s + "'"
 
   before_save :update_board_member_role
   after_save :demote_others_from_my_position
@@ -24,14 +26,30 @@ class User < ActiveRecord::Base
     first_name + ' ' + last_name
   end
 
+  def User.unique_chairs
+    chair_assignments = Assignment.find(:all, :conditions => [ "duty_id = ?", Duty.find_by_name("Chair").id], :group => "user_id")
+    uchairs = []
+    if (!chair_assignments.empty?) then
+      uchairs = chair_assignments.inject([User.find(chair_assignments[0].user_id)]) do | uchairs, chair_assignment|
+        uchairs << User.find(chair_assignment.user_id) if (uchairs.last.id != chair_assignment.user_id)
+        uchairs
+      end
+    end
+    return uchairs
+  end
+
   private
 
   def demote_others_from_my_position
     position.demote_others(self) if position
   end
 
+  def is_chair?
+    return self.assignments.exists?(:duty_id => Duty.find_by_name("Chair").id)
+  end
+
   def update_board_member_role
-    if self.assignments.exists?(:duty_id => Duty.find_by_name("Chair").id)
+    if is_chair?
       has_role!(:board_member)
     elsif self.position
       has_role!(:board_member)
