b1691e818fef6145b432b085c2fb8bbef155ee57
Chris <chris.lerum@gmail.com>
2009-09-12 16:20:53 -0400

    just sayin

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -4,6 +4,22 @@ describe "users/show.html.erb" do
   before do
     @mock_user = mock_model(User).as_null_object
     assigns[:user] = @mock_user
+    
+    # tom - this makes my unassigned method errors go away. is this the way i should be doing it?
+    template.stub(:show_invitation_link?)
+    template.stub(:show_edit_link?)
+  end
+
+  it "should display an email invitation link when show_invitation_link? is true" do
+    template.stub(:show_invitation_link?).and_return(true)
+    render
+    response.should have_selector("a", :href => invite_volunteer_path(@mock_user))
+  end
+
+  it "should not display an email invitation link when show_invitation_link? is false" do
+    template.stub(:show_invitation_link?).and_return(false)
+    render
+    response.should_not have_selector("a", :href => invite_volunteer_path(@mock_user))
   end
 
   it "should display an edit link when show_edit_link? is true" do
