63b40155c0b4c1d488851c51e546cbd4dada0043
Chris <chris.lerum@gmail.com>
2009-09-29 12:19:36 -0400

    fixed unique_users, have to see if works on postgres on heroku

--- app/controllers/board_members_controller.rb
+++ app/controllers/board_members_controller.rb
@@ -18,7 +18,7 @@ class BoardMembersController < UsersController
     # first get all the board members that have a position
     @board_members = User.all(:joins => :position, :order => :position)
     # then get all the board members that are chairs. Chairs may not have positions.
-    @chairs = User.unique_chairs
+    @chairs = User.all(:joins => :assignments, :conditions => 'assignments.duty_id = 2', :group => :user_id)
     
     respond_to do |format|
       format.html # index.html.erb
